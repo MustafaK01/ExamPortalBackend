8afb4fc77799a3233149e1d9311a5d23660dd662
MustafaK01 <mestefe119@gmail.com>
2023-04-12 05:15:21 +0300

    add for exception handle

---
+++ src/main/java/com/examportal/examportalbackend/exception/GeneralExceptionHandler.java
@@ -0,0 +1,44 @@
+package com.examportal.examportalbackend.exception;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GeneralExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        Map<String,String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(x->{
+            String fieldName = ((FieldError)x).getField();
+            String errorMsg = x.getDefaultMessage();
+            errors.put(fieldName,errorMsg);
+        });
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<?> shortUrlNotFoundException(UserAlreadyExistsException e){
+        Map<String,String> errors = new HashMap<>();
+        errors.put("Error : ",e.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(errors);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<?> codeAlreadyExistsException(UserNotFoundException e){
+        Map<String,String> errors = new HashMap<>();
+        errors.put("Error : ",e.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(errors);
+    }
+}
