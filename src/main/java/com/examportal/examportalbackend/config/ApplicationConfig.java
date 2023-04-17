11629d8b274e1cc7f792ebe054920149fa532419
MustafaK01 <mestefe119@gmail.com>
2023-04-17 23:46:27 +0300

    add for general configurations

---
+++ src/main/java/com/examportal/examportalbackend/config/ApplicationConfig.java
@@ -0,0 +1,39 @@
+package com.examportal.examportalbackend.config;
+
+import com.examportal.examportalbackend.exception.UserNotFoundException;
+import com.examportal.examportalbackend.repository.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class ApplicationConfig {
+    private final UserRepository userRepository;
+
+    public ApplicationConfig(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return username -> userRepository.findUserByUserName(username)
+                .orElseThrow(()->new UserNotFoundException("User Not Found"));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
