aafa64e01371e50be891ac7a2efd8a43a1239864
MustafaK01 <mestefe119@gmail.com>
2023-04-30 05:56:30 +0300

    edit

--- src/main/java/com/examportal/examportalbackend/model/exam/Question.java
+++ src/main/java/com/examportal/examportalbackend/model/exam/Question.java
@@ -32,6 +32,102 @@ public class Question {
     @JoinColumn(name = "quizId")
     private Quiz quiz;
 
+    public Question() {
+    }
 
 
+    public Question(Long questionId, String questionContent, String image,
+                    String option, String option1, String option2,
+                    String option3, String option4, String answer, Quiz quiz) {
+        this.questionId = questionId;
+        this.questionContent = questionContent;
+        this.image = image;
+        this.option = option;
+        this.option1 = option1;
+        this.option2 = option2;
+        this.option3 = option3;
+        this.option4 = option4;
+        this.answer = answer;
+        this.quiz = quiz;
+    }
+
+    public Long getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(Long questionId) {
+        this.questionId = questionId;
+    }
+
+    public String getQuestionContent() {
+        return questionContent;
+    }
+
+    public void setQuestionContent(String questionContent) {
+        this.questionContent = questionContent;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getOption() {
+        return option;
+    }
+
+    public void setOption(String option) {
+        this.option = option;
+    }
+
+    public String getOption1() {
+        return option1;
+    }
+
+    public void setOption1(String option1) {
+        this.option1 = option1;
+    }
+
+    public String getOption2() {
+        return option2;
+    }
+
+    public void setOption2(String option2) {
+        this.option2 = option2;
+    }
+
+    public String getOption3() {
+        return option3;
+    }
+
+    public void setOption3(String option3) {
+        this.option3 = option3;
+    }
+
+    public String getOption4() {
+        return option4;
+    }
+
+    public void setOption4(String option4) {
+        this.option4 = option4;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public Quiz getQuiz() {
+        return quiz;
+    }
+
+    public void setQuiz(Quiz quiz) {
+        this.quiz = quiz;
+    }
 }
